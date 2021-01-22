4420c907228fedda82ce82d05e3ccf2a124eaf27
György Kőszeg <koszeggy@gmail.com>
2021-01-22 17:08:34 +0100

    Adding GrowOnlyDictionary

--- KGySoft.CoreLibraries.PerformanceTest/PerformanceTests/Collections/ThreadSafeDictionaryPerformanceTest.cs
+++ KGySoft.CoreLibraries.PerformanceTest/PerformanceTests/Collections/ThreadSafeDictionaryPerformanceTest.cs
@@ -37,25 +37,54 @@ namespace KGySoft.CoreLibraries.PerformanceTests.Collections
     [TestFixture]
     public class ThreadSafeDictionaryPerformanceTest
     {
+        #region Nested Classes
+
+        private sealed class PoorHashTest
+        {
+            #region Properties
+
+            private readonly int value;
+
+            #endregion
+
+            #region Constructors
+
+            internal PoorHashTest(int i) => value = i;
+
+            #endregion
+
+            #region Methods
+
+            public override int GetHashCode() => 0;
+
+            public override bool Equals(object? obj) => obj is PoorHashTest pht && pht.value == value;
+
+            #endregion
+        }
+
+        #endregion
+
         #region Methods
 
         [Test]
         public void AddTest()
         {
-            const int count = 1_000_000;
+            const int count = 10_000_000;
             var dict = new Dictionary<int, object>();
             var lDict = new LockingDictionary<int, object>();
 #if !NET35
             var cDict = new ConcurrentDictionary<int, object>(dict);
 #endif
             var tDict = new ThreadSafeDictionary<int, object>(dict, strategy: HashingStrategy.And);
+            var gDict = new GrowOnlyDictionary<int, object>(count, null, true);
 
-            new IteratorPerformanceTest { Iterations = count/*, Repeat = 5*/, TestName = "Sequential Add" }
-                .AddCase(i => lDict[i] = null, "LockingDictionary")
+            new IteratorPerformanceTest { Iterations = count/*, Repeat = 5*/, WarmUp = false, TestName = "Sequential Add" }
+                .AddCase(i => lDict.Add(i, null), "LockingDictionary")
 #if !NET35
-                .AddCase(i => cDict[i] = null, "ConcurrentDictionary")
+                .AddCase(i => cDict.TryAdd(i, null), "ConcurrentDictionary")
 #endif
-                .AddCase(i => tDict[i] = null, "ThreadSafeDictionary")
+                .AddCase(i => tDict.Add(i, null), "ThreadSafeDictionary")
+                .AddCase(i => gDict.TryAdd(i, null), "GrowOnlyDictionary")
                 .DoTest()
                 .DumpResults(Console.Out);
         }
@@ -64,20 +93,98 @@ namespace KGySoft.CoreLibraries.PerformanceTests.Collections
         [Test]
         public void AddTestParallel()
         {
-            const int count = 1_000_000;
+            const int count = 10_000_000;
             var dict = new Dictionary<int, object>();
             var lDict = new LockingDictionary<int, object>();
             var cDict = new ConcurrentDictionary<int, object>(dict);
             var tDict = new ThreadSafeDictionary<int, object>(dict, strategy: HashingStrategy.And);
+            var gDict = new GrowOnlyDictionary<int, object>(count, null, true);
 
-            new PerformanceTest { Iterations = 1, CpuAffinity = null, TestName = "Parallel Add" }
-                .AddCase(() => Parallel.For(0, count, i => lDict[i] = i), "LockingDictionary")
-                .AddCase(() => Parallel.For(0, count, i => cDict[i] = i), "ConcurrentDictionary")
-                .AddCase(() => Parallel.For(0, count, i => tDict[i] = i), "ThreadSafeDictionary")
+            new PerformanceTest { Iterations = 1, CpuAffinity = null, TestName = "Parallel Add", WarmUp = false }
+                .AddCase(() => Parallel.For(0, count, i => lDict.Add(i, i)), "LockingDictionary")
+                .AddCase(() => Parallel.For(0, count, i => cDict.TryAdd(i, i)), "ConcurrentDictionary")
+                .AddCase(() => Parallel.For(0, count, i => tDict.Add(i, i)), "ThreadSafeDictionary")
+                .AddCase(() => Parallel.For(0, count, i => gDict.TryAdd(i, i)), "GrowOnlyDictionary")
                 .DoTest()
                 .DumpResults(Console.Out);
         }
 #endif
+
+        [Test]
+        public void AccessWellDistributedKeysTest()
+        {
+            const int count = 1_000_000;
+            var seq = Enumerable.Range(0, count);
+            var dict = seq.ToDictionary(i => i, i => (object)i);
+            var lDict = new LockingDictionary<int, object>(new Dictionary<int, object>(dict));
+#if !NET35
+            var cDict = new ConcurrentDictionary<int, object>(dict);
+#endif
+            var tDict = new ThreadSafeDictionary<int, object>(dict, strategy: HashingStrategy.And);
+            var gDict = new GrowOnlyDictionary<int, object>(count, null, true);
+            for (int i = 0; i < count; i++)
+                gDict[i] = null;
+
+            new IteratorPerformanceTest<object> { Iterations = count, Repeat = 5, TestName = "Sequential" }
+                .AddCase(i => lDict[i], "LockingDictionary")
+#if !NET35
+                .AddCase(i => cDict[i], "ConcurrentDictionary")
+#endif
+                .AddCase(i => tDict[i], "ThreadSafeDictionary")
+                .AddCase(i => gDict[i], "GrowOnlyDictionary")
+                .DoTest()
+                .DumpResults(Console.Out);
+
+#if !NET35
+            new PerformanceTest { Iterations = 1, Repeat = 5, CpuAffinity = null, TestName = "Parallel" }
+                .AddCase(() => Parallel.For(0, count, i => { var _ = lDict[i]; }), "LockingDictionary")
+                .AddCase(() => Parallel.For(0, count, i => { var _ = cDict[i]; }), "ConcurrentDictionary")
+                .AddCase(() => Parallel.For(0, count, i => { var _ = tDict[i]; }), "ThreadSafeDictionary")
+                .AddCase(() => Parallel.For(0, count, i => { var _ = gDict[i]; }), "GrowOnlyDictionary")
+                .DoTest()
+                .DumpResults(Console.Out);
+#endif
+        }
+
+        [Test]
+        public void AccessCollidingKeysTest()
+        {
+            const int count = 1000;
+            const int iterations = 10_000;
+
+            var seq = Enumerable.Range(0, count);
+            var dict = seq.ToDictionary(i => new PoorHashTest(i), i => i);
+            var lDict = new LockingDictionary<PoorHashTest, int>(new Dictionary<PoorHashTest, int>(dict));
+#if !NET35
+            var cDict = new ConcurrentDictionary<PoorHashTest, int>(dict);
+#endif
+            var tDict = new ThreadSafeDictionary<PoorHashTest, int>(dict, strategy: HashingStrategy.And);
+            var gDict = new GrowOnlyDictionary<PoorHashTest, int>(count, null, true);
+            foreach (var item in dict)
+                gDict[item.Key] = item.Value;
+
+            // some dictionaries insert colliding items at the first position, others at the last so making lookup equally hard all for them
+            var key = new PoorHashTest(count / 2);
+            new IteratorPerformanceTest<int> { Iterations = iterations, Repeat = 5, TestName = "Sequential" }
+                .AddCase(i => lDict[key], "LockingDictionary")
+#if !NET35
+                .AddCase(i => cDict[key], "ConcurrentDictionary")
+#endif
+                .AddCase(i => tDict[key], "ThreadSafeDictionary")
+                .AddCase(i => gDict[key], "GrowOnlyDictionary")
+                .DoTest()
+                .DumpResults(Console.Out);
+
+#if !NET35
+            new PerformanceTest { Iterations = 1, Repeat = 5, CpuAffinity = null, TestName = "Parallel" }
+                .AddCase(() => Parallel.For(0, iterations, i => { var _ = lDict[key]; }), "LockingDictionary")
+                .AddCase(() => Parallel.For(0, iterations, i => { var _ = cDict[key]; }), "ConcurrentDictionary")
+                .AddCase(() => Parallel.For(0, iterations, i => { var _ = tDict[key]; }), "ThreadSafeDictionary")
+                .AddCase(() => Parallel.For(0, iterations, i => { var _ = gDict[key]; }), "GrowOnlyDictionary")
+                .DoTest()
+                .DumpResults(Console.Out);
+#endif
+        }
 
         [Test]
         public void UpdateTest()
@@ -89,7 +196,6 @@ namespace KGySoft.CoreLibraries.PerformanceTests.Collections
 #if !NET35
             var cDict = new ConcurrentDictionary<int, object>(dict);
 #endif
-
             var tDict = new ThreadSafeDictionary<int, object>(dict, strategy: HashingStrategy.And);
 
             new IteratorPerformanceTest { Iterations = count, Repeat = 5, TestName = "Sequential Update" }
@@ -122,7 +228,6 @@ namespace KGySoft.CoreLibraries.PerformanceTests.Collections
 #if !NET35
             var cDict = new ConcurrentDictionary<int, object>(dict);
 #endif
-
             var tDict = new ThreadSafeDictionary<int, object>(dict, strategy: HashingStrategy.And);
 
             new IteratorPerformanceTest<object> { Iterations = count, Repeat = 5, TestName = "Sequential Update" }
@@ -163,38 +268,6 @@ namespace KGySoft.CoreLibraries.PerformanceTests.Collections
                     tDict[i] = i;
                     tDict.TryGetValue(i, out var _);
                 }), "ThreadSafeDictionary")
-                .DoTest()
-                .DumpResults(Console.Out);
-#endif
-        }
-
-        [Test]
-        public void AccessTest()
-        {
-            const int count = 1_000_000;
-            var seq = Enumerable.Range(0, count);
-            var dict = seq.ToDictionary(i => i, i => (object)null);
-            var lDict = new LockingDictionary<int, object>(new Dictionary<int, object>(dict));
-#if !NET35
-            var cDict = new ConcurrentDictionary<int, object>(dict);
-#endif
-            var tDict = new ThreadSafeDictionary<int, object>(dict, strategy: HashingStrategy.And);
-            //var tDict1 = new ThreadSafeDictionary1<int, object>(dict, strategy: HashingStrategy.And);
-
-            new IteratorPerformanceTest<object> { Iterations = count, Repeat = 5, TestName = "Sequential" }
-                .AddCase(i => lDict[i], "LockingDictionary")
-#if !NET35
-                .AddCase(i => cDict[i], "ConcurrentDictionary")
-#endif
-                .AddCase(i => tDict[i], "ThreadSafeDictionary")
-                .DoTest()
-                .DumpResults(Console.Out);
-
-#if !NET35
-            new PerformanceTest { Iterations = 1, Repeat = 5, CpuAffinity = null, TestName = "Parallel" }
-                .AddCase(() => Parallel.For(0, count, i => { var _ = lDict[i]; }), "LockingDictionary")
-                .AddCase(() => Parallel.For(0, count, i => { var _ = cDict[i]; }), "ConcurrentDictionary")
-                .AddCase(() => Parallel.For(0, count, i => { var _ = tDict[i]; }), "ThreadSafeDictionary")
                 .DoTest()
                 .DumpResults(Console.Out);
 #endif
---
+++ KGySoft.CoreLibraries/Collections/GrowOnlyDictionary.cs
@@ -0,0 +1,261 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: GrowOnlyDictionary.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2021 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution. If not, then this file is considered as
+//  an illegal copy.
+//
+//  Unauthorized copying of this file, via any medium is strictly prohibited.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+using KGySoft.CoreLibraries;
+
+#endregion
+
+namespace KGySoft.Collections
+{
+    /// <summary>
+    /// Represents a lock-free thread safe dictionary where items can only be added and queried but not replaced or deleted.
+    /// </summary>
+    internal class GrowOnlyDictionary<TKey, TValue>
+        where TKey : notnull
+    {
+        #region Nested types
+
+        #region Entry class
+
+        private sealed class Entry
+        {
+            #region Fields
+
+            internal uint Hash;
+            internal TKey Key;
+            internal TValue Value;
+            internal volatile Entry? Next;
+
+            #endregion
+
+            #region Constructors
+
+            internal Entry(uint hashCode, TKey key, TValue value)
+            {
+                Hash = hashCode;
+                Key = key;
+                Value = value;
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Bucket struct
+
+        /// <summary>
+        /// Just a volatile reference holder so array items can be treated volatile.
+        /// This is needed because in older targeted platforms there is no generic Volatile.Read/Write
+        /// </summary>
+        private struct Bucket
+        {
+            #region Fields
+
+            internal volatile Entry? First;
+
+            #endregion
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Fields
+
+        #region Static Fields
+
+        private static readonly IEqualityComparer<TKey> defaultComparer = ComparerHelper<TKey>.EqualityComparer;
+
+        #endregion
+
+        #region Instance Fields
+
+        private readonly IEqualityComparer<TKey>? comparer;
+        private readonly bool isAndHash;
+        private readonly Bucket[] buckets;
+
+        private uint hashingOperand; // buckets.Length - 1 for AND hashing, buckets.Length for MOD hashing
+        private int count;
+
+        #endregion
+
+        #endregion
+
+        #region Properties and Indexers
+
+        #region Properties
+
+        public int Count => Volatile.Read(ref count);
+
+        #endregion
+
+        #region Indexers
+
+        public TValue this[TKey key]
+        {
+            get => TryGetValue(key, out TValue value) ? value : Throw.KeyNotFoundException<TValue>(Res.IDictionaryKeyNotFound);
+            set
+            {
+                if (key == null!)
+                    Throw.ArgumentNullException(Argument.key);
+                TryAddInternal(key, value, GetHashCode(key));
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructors
+
+        internal GrowOnlyDictionary(int capacity, IEqualityComparer<TKey>? comparer, bool isAndHash)
+        {
+            this.comparer = comparer;
+            this.isAndHash = isAndHash;
+            buckets = new Bucket[GetBucketSize(capacity)];
+        }
+
+        #endregion
+
+        #region Methods
+
+        #region Public Methods
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            if (key == null!)
+                Throw.ArgumentNullException(Argument.key);
+            return TryAddInternal(key, value, GetHashCode(key));
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            if (key == null!)
+                Throw.ArgumentNullException(Argument.key);
+            return TryGetValueInternal(key, GetHashCode(key), out value);
+        }
+
+        #endregion
+
+        #region Internal Methods
+
+        internal bool TryAddInternal(TKey key, TValue value, uint hashCode)
+        {
+            Debug.Assert(key != null!);
+            ref Bucket bucketRef = ref buckets[GetBucketIndex(hashCode)];
+
+            // we are optimistic
+            var newEntry = new Entry(hashCode, key, value);
+
+            // bucket is empty: trying to add the first item
+            if (bucketRef.First == null)
+            {
+                if (Interlocked.CompareExchange(ref bucketRef.First, newEntry, null) == null)
+                {
+                    Interlocked.Increment(ref count);
+                    return true;
+                }
+
+                // Here we have a lost race. Though Bucket is a struct, detecting change works because we stored a ref to the bucket.
+            }
+
+            // iterating through the entries until we find key or the end of the list
+            IEqualityComparer<TKey> comp = comparer ?? defaultComparer;
+            Entry entry = bucketRef.First;
+            while (true)
+            {
+                // key already exists
+                if (entry.Hash == hashCode && comp.Equals(key, entry.Key))
+                    return false;
+
+                // last item in the bucket: trying to chain the new one
+                if (entry.Next == null)
+                {
+                    if (Interlocked.CompareExchange(ref entry.Next, newEntry, null) == null)
+                    {
+                        Interlocked.Increment(ref count);
+                        return true;
+                    }
+                }
+
+                entry = entry.Next;
+            }
+        }
+
+        [MethodImpl(MethodImpl.AggressiveInlining)]
+        internal bool TryGetValueInternal(TKey key, uint hashCode, [MaybeNullWhen(false)] out TValue value)
+        {
+            Debug.Assert(key != null!);
+
+            IEqualityComparer<TKey> comp = comparer ?? defaultComparer;
+            for (Entry? entry = buckets[GetBucketIndex(hashCode)].First; entry != null; entry = entry.Next)
+            {
+                if (entry.Hash == hashCode && comp.Equals(key, entry.Key))
+                {
+                    value = entry.Value;
+                    return true;
+                }
+            }
+
+            value = default;
+            return false;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private uint GetBucketSize(int capacity)
+        {
+            if (isAndHash)
+            {
+                uint bucketSize = (uint)Math.Max(2, capacity).GetNextPowerOfTwo();
+                hashingOperand = bucketSize - 1;
+                return bucketSize;
+            }
+
+            return hashingOperand = (uint)PrimeHelper.GetPrime(capacity);
+        }
+
+        /// <summary>
+        /// An if in a non-virtual method is still faster than calling an abstract method, a delegate or even a C# 9 function pointer
+        /// </summary>
+        [MethodImpl(MethodImpl.AggressiveInlining)]
+        private uint GetBucketIndex(uint hashCode) => isAndHash
+            ? hashCode & hashingOperand
+            : hashCode % hashingOperand;
+
+        [MethodImpl(MethodImpl.AggressiveInlining)]
+        private uint GetHashCode(TKey key)
+        {
+            IEqualityComparer<TKey>? comp = comparer;
+            return (uint)(comp == null ? key.GetHashCode() : comp.GetHashCode(key));
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
