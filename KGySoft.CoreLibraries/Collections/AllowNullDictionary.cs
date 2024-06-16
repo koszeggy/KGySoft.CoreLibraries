2b940589ee6f755bd141d3333c6c43591a4267d5
György Kőszeg <koszeggy@yahoo.com>
2024-06-16 20:13:37 +0200

    Making AllowNullDictionary public

--- KGySoft.CoreLibraries/Collections/AllowNullDictionary.cs
+++ KGySoft.CoreLibraries/Collections/AllowNullDictionary.cs
@@ -20,9 +20,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 using KGySoft.Annotations;
 using KGySoft.Diagnostics;
+using KGySoft.Reflection;
 
 #endregion
 
@@ -31,23 +33,24 @@ using KGySoft.Diagnostics;
 #if NETCOREAPP3_0_OR_GREATER
 #pragma warning disable CS8714 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'notnull' constraint. - TKey CAN be null here
 #else
-#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes). - That's the point
 #endif
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable. - False alarm, it is decorated by [AllowNull]. Declaring as nullable would just raise more false alarm warnings.
 
 #endregion
-
 
 namespace KGySoft.Collections
 {
     /// <summary>
-    /// Represents a dictionary, which allows <see langword="null"/> as a key.
+    /// Represents a dictionary that allows <see langword="null"/> as a key.
     /// </summary>
     /// <typeparam name="TKey">The type of the key.</typeparam>
     /// <typeparam name="TValue">The type of the value.</typeparam>
     /// <seealso cref="IDictionary{TKey, TValue}" />
     [Serializable]
     [DebuggerTypeProxy(typeof(DictionaryDebugView<,>))]
-    internal class AllowNullDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    [SuppressMessage("ReSharper", "UseNullableReferenceTypesAnnotationSyntax", Justification = "False alarm, only [NotNull] prevents AssignNullToNotNullAttribute warnings")]
+    public class AllowNullDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
         #region Constants
 
@@ -60,20 +63,30 @@ namespace KGySoft.Collections
         private readonly Dictionary<TKey, TValue> dict;
 
         private bool hasNullKey;
-
-        // ReSharper disable once RedundantDefaultMemberInitializer - needed for constructor
-        [AllowNull]private TValue nullValue = default!;
+        [AllowNull]private TValue nullValue;
 
         #endregion
 
         #region Properties and Indexers
 
         #region Properties
+        
+        #region Public Properties
 
+        /// <summary>
+        /// Gets the keys stored in the dictionary.
+        /// </summary>
+        /// <remarks>
+        /// <para>The returned <see cref="ICollection{T}"/> is not a static copy; instead, the <see cref="ICollection{T}"/> refers back to the keys in the original <see cref="AllowNullDictionary{TKey,TValue}"/>.
+        /// Therefore, changes to the <see cref="AllowNullDictionary{TKey,TValue}"/> continue to be reflected in the <see cref="ICollection{T}"/>.</para>
+        /// <para>Retrieving the value of this property is an O(1) operation.</para>
+        /// <note>The enumerator of the returned collection does not support the <see cref="IEnumerator.Reset">IEnumerator.Reset</see> method.</note>
+        /// </remarks>
         public ICollection<TKey> Keys
         {
             get
             {
+                // TOTO: not a snapshot
                 if (!hasNullKey)
                     return dict.Keys;
 
@@ -84,10 +97,20 @@ namespace KGySoft.Collections
             }
         }
 
+        /// <summary>
+        /// Gets the values stored in the dictionary.
+        /// </summary>
+        /// <remarks>
+        /// <para>The returned <see cref="ICollection{T}"/> is not a static copy; instead, the <see cref="ICollection{T}"/> refers back to the values in the original <see cref="AllowNullDictionary{TKey,TValue}"/>.
+        /// Therefore, changes to the <see cref="AllowNullDictionary{TKey,TValue}"/> continue to be reflected in the <see cref="ICollection{T}"/>.</para>
+        /// <para>Retrieving the value of this property is an O(1) operation.</para>
+        /// <note>The enumerator of the returned collection does not support the <see cref="IEnumerator.Reset">IEnumerator.Reset</see> method.</note>
+        /// </remarks>
         public ICollection<TValue> Values
         {
             get
             {
+                // TOTO: not a snapshot
                 if (!hasNullKey)
                     return dict.Values;
 
@@ -98,14 +121,41 @@ namespace KGySoft.Collections
             }
         }
 
+        /// <summary>
+        /// Gets number of elements currently stored in this <see cref="AllowNullDictionary{TKey,TValue}"/> instance.
+        /// </summary>
         public int Count => hasNullKey ? dict.Count + 1 : dict.Count;
 
-        public bool IsReadOnly => false;
+        #endregion
+
+        #region Explicitly Implemented Interface Properties
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly => false;
+
+        #endregion
 
         #endregion
 
         #region Indexers
 
+        /// <summary>
+        /// Gets or sets the value associated with the specified <paramref name="key"/>.
+        /// </summary>
+        /// <returns>
+        /// The element with the specified <paramref name="key"/>.
+        /// </returns>
+        /// <param name="key">The key of the value to get or set. In this dictionary it can be even <see langword="null"/>.</param>
+        /// <exception cref="KeyNotFoundException">The property is retrieved and <paramref name="key"/> is not found.</exception>
+        /// <remarks>
+        /// <para>In this dictionary both the <paramref name="key"/> and the <paramref name="value"/>can be <see langword="null"/>.</para>
+        /// <para>If the <paramref name="key"/> is not found when a value is being retrieved, <see cref="KeyNotFoundException"/> is thrown.
+        /// If the key is not found when a value is being set, the key and value are added.</para>
+        /// <para>You can also use this property to add new elements by setting the value of a key that does not exist in the <see cref="AllowNullDictionary{TKey,TValue}"/>, for example:
+        /// <code lang="C#">myDictionary[myNonexistentKey] = myValue;</code>
+        /// However, if the specified key already exists in the <see cref="AllowNullDictionary{TKey,TValue}"/>, setting this property
+        /// overwrites the old value. In contrast, the <see cref="Add">Add</see> method throws an <see cref="ArgumentException"/>, when <paramref name="key"/> already exists in the collection.</para>
+        /// <para>Getting or setting this property approaches an O(1) operation.</para>
+        /// </remarks>
         public TValue this[[CanBeNull]TKey key]
         {
             get
@@ -135,13 +185,43 @@ namespace KGySoft.Collections
 
         #region Constructors
 
-        public AllowNullDictionary(int capacity) => dict = new Dictionary<TKey, TValue>(capacity);
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AllowNullDictionary{TKey,TValue}"/> class.
+        /// </summary>
         public AllowNullDictionary() : this(defaultCapacity)
         {
         }
 
-        public AllowNullDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection) : this(((collection as ICollection<KeyValuePair<TKey, TValue>>)?.Count).GetValueOrDefault(defaultCapacity))
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AllowNullDictionary{TKey,TValue}"/> class
+        /// using the specified <paramref name="comparer"/>.
+        /// </summary>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing keys.</param>
+        public AllowNullDictionary(IEqualityComparer<TKey>? comparer) : this(defaultCapacity, comparer)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AllowNullDictionary{TKey,TValue}"/> class
+        /// using the specified <paramref name="capacity"/> and <paramref name="comparer"/>.
+        /// </summary>
+        /// <param name="capacity">The initial capacity that the <see cref="AllowNullDictionary{TKey,TValue}"/> can contain.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing keys. This parameter is optional.
+        /// <br/>Default value: <see langword="null"/>.</param>
+        public AllowNullDictionary(int capacity, IEqualityComparer<TKey>? comparer = null)
+            => dict = new Dictionary<TKey, TValue>(capacity, comparer);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AllowNullDictionary{TKey,TValue}"/> class
+        /// with the specified <paramref name="collection"/>.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing keys. This parameter is optional.
+        /// <br/>Default value: <see langword="null"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="collection"/> contains one or more duplicate keys.</exception>
+        public AllowNullDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection, IEqualityComparer<TKey>? comparer = null)
+            : this(((collection as ICollection<KeyValuePair<TKey, TValue>>)?.Count).GetValueOrDefault(defaultCapacity), comparer)
         {
             if (collection == null!)
                 Throw.ArgumentNullException(Argument.collection);
@@ -155,6 +235,22 @@ namespace KGySoft.Collections
 
         #region Public Methods
 
+        /// <summary>
+        /// Adds an element with the provided key and value to the <see cref="AllowNullDictionary{TKey,TValue}"/>.
+        /// </summary>
+        /// <param name="key">The key of the element to add. In this dictionary it can be even <see langword="null"/>.</param>
+        /// <param name="value">The value of the element to add.</param>
+        /// <exception cref="ArgumentException">An element with the same key already exists in the <see cref="AllowNullDictionary{TKey,TValue}"/>.</exception>
+        /// <remarks>
+        /// <para>In this dictionary both the <paramref name="key"/> and the <paramref name="value"/>can be <see langword="null"/>.</para>
+        /// <para>You can also use the <see cref="this[TKey]">indexer</see> to add new elements by setting the value of a
+        /// key that does not exist in the <see cref="AllowNullDictionary{TKey,TValue}"/>. for example:
+        /// <code lang="C#"><![CDATA[myCollection[myNonexistentKey] = myValue;]]></code>
+        /// However, if the specified key already exists in the <see cref="AllowNullDictionary{TKey,TValue}"/>, setting the <see cref="this[TKey]">indexer</see>
+        /// overwrites the old value. In contrast, the <see cref="Add">Add</see> method does not modify existing elements.</para>
+        /// <para>This method approaches an O(1) operation unless if insertion causes a resize, in which case the operation is O(n).</para>
+        /// </remarks>
+        [MethodImpl(MethodImpl.AggressiveInlining)]
         public void Add([CanBeNull]TKey key, TValue value)
         {
             if (key != null)
@@ -169,48 +265,85 @@ namespace KGySoft.Collections
             nullValue = value;
         }
 
+        /// <summary>
+        /// Determines whether the <see cref="AllowNullDictionary{TKey,TValue}"/> contains a specific key.
+        /// </summary>
+        /// <param name="key">The key to locate in the <see cref="AllowNullDictionary{TKey,TValue}"/>.</param>
+        /// <returns><see langword="true"/> if the <see cref="AllowNullDictionary{TKey,TValue}"/> contains an element with the specified <paramref name="key"/>; otherwise, <see langword="false"/>.</returns>
+        /// <remarks><para>This method approaches an O(1) operation.</para></remarks>
+        [MethodImpl(MethodImpl.AggressiveInlining)]
         public bool ContainsKey([CanBeNull]TKey key) => key == null ? hasNullKey : dict.ContainsKey(key);
 
+        /// <summary>
+        /// Removes the value with the specified <paramref name="key"/> from the <see cref="AllowNullDictionary{TKey,TValue}"/>.
+        /// </summary>
+        /// <param name="key">Key of the item to remove.</param>
+        /// <returns><see langword="true"/> if the element is successfully removed; otherwise, <see langword="false"/>. This method also returns <see langword="false"/> if key was not found in the dictionary.</returns>
+        /// <remarks><para>If the <see cref="AllowNullDictionary{TKey,TValue}"/> does not contain an element with the specified key, the dictionary remains unchanged. No exception is thrown.</para>
+        /// <para>This method approaches an O(1) operation.</para>
+        /// </remarks>
+        [MethodImpl(MethodImpl.AggressiveInlining)]
         public bool Remove([CanBeNull]TKey key)
         {
             if (key != null)
                 return dict.Remove(key);
             bool oldHasNull = hasNullKey;
             hasNullKey = false;
+            if (Reflector<TValue>.IsManaged)
+                nullValue = default;
             return oldHasNull;
         }
 
+        /// <summary>
+        /// Tries to get the value associated with the specified <paramref name="key"/>.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/>, if this dictionary contains an element with the specified key; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <param name="key">The key whose value to get.</param>
+        /// <param name="value">When this method returns, the value associated with the specified key, if the <paramref name="key"/> is found;
+        /// otherwise, the default value for the type of the <paramref name="value"/> parameter. This parameter is passed uninitialized.</param>
+        /// <remarks>
+        /// <para>If the <paramref name="key"/> is not found, then the <paramref name="value"/> parameter gets the appropriate default value
+        /// for the type <typeparamref name="TValue"/>; for example, 0 (zero) for integer types, <see langword="false"/> for Boolean types, and <see langword="null"/> for reference types.</para>
+        /// <para>This method approaches an O(1) operation.</para>
+        /// </remarks>
+        [MethodImpl(MethodImpl.AggressiveInlining)]
         public bool TryGetValue([CanBeNull]TKey key, [MaybeNullWhen(false)]out TValue value)
         {
             if (key != null)
                 return dict.TryGetValue(key, out value);
 
-            value = hasNullKey ? nullValue : default(TValue)!;
+            value = hasNullKey ? nullValue : default;
             return hasNullKey;
         }
 
+        /// <summary>
+        /// Removes all keys and values from the <see cref="AllowNullDictionary{TKey,TValue}"/>.
+        /// </summary>
+        /// <remarks>
+        /// <para>The <see cref="Count"/> property is set to 0, and references to other objects from elements of the collection are also released.</para>
+        /// <para>This method is an O(1) operation.</para>
+        /// </remarks>
         public void Clear()
         {
             hasNullKey = false;
-            nullValue = default(TValue);
+            if (Reflector<TValue>.IsManaged)
+                nullValue = default;
             dict.Clear();
         }
 
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-        {
-            if (array == null!)
-                Throw.ArgumentNullException(Argument.array);
-            if (arrayIndex < 0 || arrayIndex > array.Length)
-                Throw.ArgumentOutOfRangeException(Argument.arrayIndex);
-            if (array.Length - arrayIndex < Count)
-                Throw.ArgumentException(Argument.array, Res.ICollectionCopyToDestArrayShort);
-
-            ((ICollection<KeyValuePair<TKey, TValue>>)dict).CopyTo(array, arrayIndex);
-            if (hasNullKey)
-                array[arrayIndex + dict.Count] = new KeyValuePair<TKey, TValue>(default(TKey)!, nullValue);
-        }
-
+        /// <summary>
+        /// Returns an enumerator that iterates through the <see cref="AllowNullDictionary{TKey,TValue}"/>.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the <see cref="AllowNullDictionary{TKey,TValue}"/>.
+        /// </returns>
+        /// <remarks>
+        /// <note>The returned enumerator supports the <see cref="IEnumerator.Reset">IEnumerator.Reset</see> method.</note>
+        /// </remarks>
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+            // TODO: value type enumerator
             => !hasNullKey ? dict.GetEnumerator() : GetEnumeratorWithNull();
 
         #endregion
@@ -219,7 +352,7 @@ namespace KGySoft.Collections
 
         private IEnumerator<KeyValuePair<TKey, TValue>> GetEnumeratorWithNull()
         {
-            yield return new KeyValuePair<TKey, TValue>(default(TKey)!, nullValue);
+            yield return new KeyValuePair<TKey, TValue>(default!, nullValue);
             foreach (var item in dict)
                 yield return item;
         }
@@ -237,6 +370,20 @@ namespace KGySoft.Collections
 
         bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
             => TryGetValue(item.Key, out TValue? value) && EqualityComparer<TValue>.Default.Equals(value, item.Value) && Remove(item.Key);
+
+        void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            if (array == null!)
+                Throw.ArgumentNullException(Argument.array);
+            if (arrayIndex < 0 || arrayIndex > array.Length)
+                Throw.ArgumentOutOfRangeException(Argument.arrayIndex);
+            if (array.Length - arrayIndex < Count)
+                Throw.ArgumentException(Argument.array, Res.ICollectionCopyToDestArrayShort);
+
+            if (hasNullKey)
+                array[arrayIndex] = new KeyValuePair<TKey, TValue>(default!, nullValue);
+            ((ICollection<KeyValuePair<TKey, TValue>>)dict).CopyTo(array, arrayIndex + (hasNullKey ? 1 : 0));
+        }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
